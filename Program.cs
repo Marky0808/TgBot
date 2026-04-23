b07f9c21915849e5ccdc31ea6655ab03969574aa
Marky0808 <todikashmangust@gmail.com>
2026-04-24 00:10:45 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ using Telegram.Bot.Types.Enums;
 class Program
 {
     private static readonly string BotToken = "8659929475:AAHmXVxlel6mVSSJ7Ik1jVbZcvKp4u8FBWs";
-
+    
     static async Task Main(string[] args)
     {
         var botClient = new TelegramBotClient(BotToken);
@@ -20,7 +20,7 @@ class Program
         
         var receiverOptions = new ReceiverOptions
         {
-            AllowedUpdates = Array.Empty<UpdateType>() // Отримувати всі типи оновлень
+            AllowedUpdates = Array.Empty<UpdateType>()
         };
         
         botClient.StartReceiving(
@@ -62,5 +62,13 @@ class Program
 
         Console.WriteLine(ErrorMessage);
         return Task.CompletedTask;
+    }
+
+    static async Task<string> TranslateTextAsync(string textToTranslate, string targetLanguage)
+    {
+        // реалізувати HTTP-запит до API перекладача.
+        
+        await Task.Delay(500); // Імітація затримки мережі
+        return $"[Переклад на {targetLanguage}]: {textToTranslate}";
     }
 }
